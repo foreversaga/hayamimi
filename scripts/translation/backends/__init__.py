b546932052b0819c5f843415f36d9f836358b502
Barry Chiu <53308160+foreversaga@users.noreply.github.com>
2026-08-26 10:01:34 +0800

    feat(translation): export translation backends

---
+++ scripts/translation/backends/__init__.py
@@ -0,0 +1,7 @@
+from .openai_compatible import (
+    HyMT2OpenAIBackend,
+    OpenAICompatibleConfig,
+    TranslationBackendError,
+)
+
+__all__ = ["HyMT2OpenAIBackend", "OpenAICompatibleConfig", "TranslationBackendError"]
